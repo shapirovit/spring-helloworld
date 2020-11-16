ea76f8eeda44cd47797409ca8b720f711b2e03ad
Vitaliy Shapiro <shapirovet@gmail.com>
2020-11-16 08:54:29 +0300

    finish 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+
+    public Cat() {
+
+    }
+ 
+    private String message;
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+     
+}
